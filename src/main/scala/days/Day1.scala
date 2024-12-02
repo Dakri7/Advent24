664b343f8a9bc85c6f68eb9bc3908427af52b51f
dakri7 <daniel.krimm@stud.tu-darmstadt.de>
2024-12-02 20:03:25 +0100

    Day 1

--- src/main/scala/days/Day1.scala
+++ src/main/scala/days/Day1.scala
@@ -4,6 +4,14 @@ package days
 import utils.Utils
 
 object Day1 extends App {
-  (new Utils).hi()
+  val input = (new Utils).readInputsFromConsole()
+  val fst = input.map(l => l(0).toInt).sorted
+  val snd = input.map(l => l(1).toInt).sorted
+  val zipped = fst zip snd
+  val totalDist = zipped.map({case (a, b) => Math.abs(a-b)}).sum
+
+  val groupedSnd = snd.groupBy(identity).mapValues(_.size)
+  val weightedFst = fst.map(i => i * (groupedSnd.get(i) getOrElse 0))
+  println(weightedFst.sum)
 }
 
